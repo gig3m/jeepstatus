33265615c2c174de640bb141185fb81ba0efc4b6
Kyle Arrington <kyle@arrington.co>
2013-04-16 23:36:15 -0500

    Initial Commit

---
+++ public/index.php
@@ -0,0 +1,59 @@
+<?php
+require '../vendor/autoload.php';
+require '../vots.php';
+
+////////////////////////
+// Setup
+////////////////////////
+
+//Instantiate Slim
+$app = new \Slim\Slim(array(
+    'templates.path' => '../views',
+));
+
+//Twig hates the config array above
+$app->view(new \Slim\Extras\Views\Twig());
+
+////////////////////////
+// Routes
+////////////////////////
+
+//Redirect POST to get URL for bookmarking
+$app->post('/status', function () use ($app) {
+
+    $last = $_POST['last'];
+    $vin  = $_POST['vin'];
+
+    //Do something
+    $app->redirect("/status/$last/$vin/");
+
+});
+
+//Status 
+$app->get('/status/:last/:vin/', function ($last, $vin) use ($app) {
+
+    $jeep = new VOTSService($last, $vin);
+    $data = [
+        'statusCode' => $jeep->getStatusCode(),
+        'statusDesc' => $jeep->getStatusDesc(),
+        'statusExplanation' => $jeep->getStatusExplanation()
+    ];
+
+    //Do something
+    $app->render('response.twig', $data);
+
+});
+
+// // Home
+$app->get('/', function () use ($app) {
+
+    //Do something
+    $app->render('home.twig');
+
+});
+
+////////////////////////
+// Go Go
+////////////////////////
+
+$app->run();
---
+++ vots.php
@@ -0,0 +1,109 @@
+<?php
+/**
+* 
+* 
+* 
+* 
+ */
+
+class VOTSService {
+
+        public $lastname;
+        public $vin;
+        public static $decoded;
+
+        private $codes = [
+            "BB"   => "review by fleet department",
+            "BD"   => "special equipment processing",
+            "BE"   => "edit error",
+            "BG"   => "passed edit n/a for schedule",
+            "BGL"  => "edit ok parts unavailable",
+            "BX"   => "passed edit available for schedule",
+            "C"    => "sub firm",
+            "D"    => "firm schedule - dealer has allocation and all parts available",
+            "D"    => "1 gateline schedule - scheduled to be built",
+            "E"    => "frame",
+            "F"    => "paint",
+            "G"    => "trim",
+            "I"    => "built not ok'd",
+            "J"    => "built ok'd",
+            "JB"   => "shipped to body vendor",
+            "JE"   => "emission check",
+            "JS"   => "shipped to storage",
+            "KZ"   => "released by plant , invoiced",
+            "KZL"  => "released - not shipped",
+            "KZM"  => "first rail departure",
+            "KZN"  => "first rail arrival",
+            "KZO"  => "delayed/recieved",
+            "KZOA" => "plant holds",
+            "KZOB" => "zone/distribution holds",
+            "KZOC" => "carrier delays",
+            "KZOD" => "carrier holds",
+            "KZOE" => "mis-shipped vehicle",
+            "KZOF" => "show/test vehicle",
+            "KZOG" => "damaged vehicle",
+            "KZOH" => "all other reasons",
+            "KZT"  => "second rail departure",
+            "KZU"  => "second rail arrival",
+            "KZX"  => "delivered to dealer",
+            "ZA"   => "canceled",
+        ];
+
+
+    public function __construct($lastname, $vin)
+    {
+        $this->lastname = $lastname;
+        $this->vin = $vin;
+        self::$decoded = NULL;
+
+        if ($this->getJSON() != TRUE)
+        {
+            print self::$decoded['ERROR_DESC'];
+            exit;
+        }
+    }
+
+    public function getJSON()
+    {
+        $lastname = $this->lastname;
+        $vin = $this->vin;
+
+        // Get VOTS Servlet
+        $response = file_get_contents("https://www.jeep.com/vots/VOTSServlet?firstName=&lastName=$lastname&vin_last8=$vin&service=json");
+
+        //trim the crap off the front and back, wrapped in votsservice()
+        $response = rtrim(ltrim($response, "votsservice("), ")");
+
+        self::$decoded =  json_decode($response, TRUE);
+
+        if (self::$decoded['Status'] == 'SUCCESS')
+        {
+            return TRUE;   
+        }
+        else
+        {
+            return FALSE; 
+        }
+    }
+
+    public function getStatusCode()
+    {
+        return self::$decoded['StatusDetails']['cStatus'];
+    }
+
+    public function getStatusDesc()
+    {
+        return self::$decoded['StatusDetails']['statusDesc'];
+    }
+
+    public function getStatusExplanation()
+    {
+        return ucwords($this->codes[self::$decoded['StatusDetails']['cStatus']]);
+    }
+
+
+
+
+}
+
+
